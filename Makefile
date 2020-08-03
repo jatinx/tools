b7dabec2d2f80c6f909f28049817c369e18e9a3d
Jatin Chaudhary <ndjatin@gmail.com>
2020-08-03 18:30:45 +0530

    Adding Calculator

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ all:
 	mkdir -p build/
 	clang++ -O3 -Iinclude/ src/endian.cc -o build/endian
 	clang++ -O3 -Iinclude/ src/dumpbits.cc -o build/dumpbits
+	clang++ -O3 -Iinclude/ src/calc.cc -o build/calc
 
 clean:
 	rm -rf build/
---
+++ src/calc.cc
@@ -0,0 +1,34 @@
+#include "common.h"
+#include <cstring>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    error_message("Invalid Number of Arguments, usage calc 1 + 2", ACTION::DIE);
+  }
+
+  // Validate operator
+  if (strlen(argv[2]) != 1) {
+    error_message("Length of operator > 1", ACTION::DIE);
+  }
+  char o = *argv[2];
+  long long a = std::stol(argv[1]);
+  long long b = std::stol(argv[3]);
+
+  switch (o) {
+  case '+':
+    std::cout << a + b << std::endl;
+    break;
+  case '-':
+    std::cout << a - b << std::endl;
+    break;
+  case '*':
+    std::cout << a * b << std::endl;
+    break;
+  case '/':
+    std::cout << a / b << std::endl;
+    break;
+  default:
+    error_message("Supported Operators:: +, -, *, /", ACTION::DIE);
+  }
+}
